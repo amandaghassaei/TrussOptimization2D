c9c081049f150978e9d3f1ba0ff18770ac068781
amandaghassaei <amandaghassaei@gmail.com>
2016-10-31 23:26:38 -0400

    first commit

---
+++ js/controls.js
@@ -0,0 +1,165 @@
+/**
+ * Created by ghassaei on 10/31/16.
+ */
+
+
+function initControls(globals){
+
+    $("#logo").mouseenter(function(){
+        $("#activeLogo").show();
+        $("#inactiveLogo").hide();
+    });
+    $("#logo").mouseleave(function(){
+        $("#inactiveLogo").show();
+        $("#activeLogo").hide();
+    });
+
+    setLink("#about", function(){
+        $('#aboutModal').modal('show');
+    });
+
+    function updateScaleBars(min, max){
+        for (var i=0;i<=20;i++){
+            var val = (max-min)*(20-i)/20+min;
+            $("#swatch" + i).css("background", hexForVal(val, min, max));
+            if (i%5 == 0) $("#label" + i).html(val.toFixed(2));
+        }
+    }
+
+    var $moreInfo = $("#moreInfo");
+    var $moreInfoInput = $("#moreInfo>input");
+    var $moreInfoSpan = $("#moreInfo>span");
+    function showMoreInfo(string, e){
+        $moreInfo.children("span").html(string);
+        $moreInfo.css({top: e.clientY - 50, left: e.clientX + 10});
+        $moreInfoSpan.show();
+        $moreInfoInput.hide();
+        $moreInfo.show();
+    }
+    function hideMoreInfo(){
+        $moreInfo.hide();
+    }
+    function editMoreInfo(val, callback){
+        var $moreInfo = $("#moreInfo");
+        $moreInfoInput.show();
+        $moreInfoSpan.hide();
+        $moreInfoInput.focus();
+        $moreInfoInput.val(val);
+        $moreInfoInput.change(function(){
+            $moreInfoInput.hide();
+            $moreInfoInput.unbind("change");
+            $moreInfo.hide();
+            var newVal = $moreInfoInput.val();
+            if (isNaN(parseFloat(newVal))) return;
+            newVal = parseFloat(newVal);
+            callback(newVal);
+        })
+    }
+
+
+    function setLink(id, callback){
+        $(id).click(function(e){
+            e.preventDefault();
+            callback(e);
+        });
+    }
+
+    function setRadio(name, val, callback){
+        $("input[name=" + name + "]").on('change', function() {
+            var state = $("input[name="+name+"]:checked").val();
+            callback(state);
+        });
+        $(".radio>input[value="+val+"]").prop("checked", true);
+    }
+
+    function setInput(id, val, callback, min, max){
+        var $input = $(id);
+        $input.change(function(){
+            var val = $input.val();
+            if ($input.hasClass("int")){
+                if (isNaN(parseInt(val))) return;
+                val = parseInt(val);
+            } else {
+                if (isNaN(parseFloat(val))) return;
+                val = parseFloat(val);
+            }
+            if (min !== undefined && val < min) val = min;
+            if (max !== undefined && val > max) val = max;
+            $input.val(val);
+            callback(val);
+        });
+        $input.val(val);
+    }
+
+    function setCheckbox(id, state, callback){
+        var $input  = $(id);
+        $input.on('change', function () {
+            if ($input.is(":checked")) callback(true);
+            else callback(false);
+        });
+        $input.prop('checked', state);
+    }
+
+    function setSlider(id, val, min, max, incr, callback, callbackOnStop){
+        var slider = $(id).slider({
+            orientation: 'horizontal',
+            range: false,
+            value: val,
+            min: min,
+            max: max,
+            step: incr
+        });
+        slider.on("slide", function(e, ui){
+            var val = ui.value;
+            callback(val);
+        });
+        slider.on("slidestop", function(){
+            var val = slider.slider('value');
+            if (callbackOnStop) callbackOnStop(val);
+        })
+    }
+
+    function setSliderInput(id, val, min, max, incr, callback){
+
+        var slider = $(id+">div").slider({
+            orientation: 'horizontal',
+            range: false,
+            value: val,
+            min: min,
+            max: max,
+            step: incr
+        });
+
+        var $input = $(id+">input");
+        $input.change(function(){
+            var val = $input.val();
+            if ($input.hasClass("int")){
+                if (isNaN(parseInt(val))) return;
+                val = parseInt(val);
+            } else {
+                if (isNaN(parseFloat(val))) return;
+                val = parseFloat(val);
+            }
+
+            var min = slider.slider("option", "min");
+            if (val < min) val = min;
+            if (val > max) val = max;
+            $input.val(val);
+            slider.slider('value', val);
+            callback(val);
+        });
+        $input.val(val);
+        slider.on("slide", function(e, ui){
+            var val = ui.value;
+            $input.val(val);
+            callback(val);
+        });
+    }
+
+    return {
+        showMoreInfo: showMoreInfo,
+        hideMoreInfo: hideMoreInfo,
+        editMoreInfo: editMoreInfo,
+        updateScaleBars: updateScaleBars
+    }
+}
---
+++ js/main.js
@@ -0,0 +1,13 @@
+/**
+ * Created by ghassaei on 10/27/16.
+ */
+
+globals = {};
+
+$(function() {
+    console.log("hi");
+
+    var controls = initControls(globals);
+    var threeView = initThreeView(globals);
+
+});
